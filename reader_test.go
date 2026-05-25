77214530af5495d937157339c4731dd47ad256de
Connor Walstrom <walst110@umn.edu>
2026-05-24 19:06:04 -0500

    Write and check for contents in test files

--- reader_test.go
+++ reader_test.go
@@ -38,18 +38,20 @@ func Test__ReadFileResult_new(t *testing.T) {
 	if !t2res.Deep_compare(t2) {t.Errorf("t2: Deep compare returned false")}
 }
 
-func Test__ReadFile(t *testing.T) {
-
-}
-
 func writeConfigFile() (*os.File, error) {
 	file, err := os.Create("config.trem.test")
 	if err != nil {return nil, err}
 
-	// write file contents
+	file.Write([]byte(
+		"type=config\n" +
+		"dirs=%HOME/.text-reminders;\"/di re ct or y\"\n",
+	))
 
 	_, err = file.Seek(0, 0)
-	if err != nil {return nil, err}
+	if err != nil {
+		file.Close()
+		return nil, err
+	}
 	return file, nil
 }
 func Test__ReadConfigFile(t *testing.T) {
@@ -60,10 +62,22 @@ func Test__ReadConfigFile(t *testing.T) {
 	var funcmap map[string]func([]string) ReadFileResult = make(map[string]func([]string) ReadFileResult) // fill out later
 	results, readErr := ReadFile(file, funcmap)
 	if readErr != nil {t.Errorf("%v", readErr)}
+	if len(results) != 2 {t.Errorf("Results mismatch. Expected 2 results, got: %v", len(results))}
 
 	// Check the results list for correctness
-	for idx := 0; idx < len(results); idx++ {
+	for idx, res := range results {
+		switch idx {
+		case 0:
+			if res.inputs[0] != "type" {t.Errorf("keyword mismatch. Expected: type. Got: " + res.inputs[0])}
+			if res.inputs[1] != "config" {t.Errorf("input mismatch. Expected: config. Got: " + res.inputs[1])}
 
+		case 1:
+			if res.inputs[0] != "dirs" {t.Errorf("keyword mismatch. Expected: . Got: " + res.inputs[0])}
+			if res.inputs[1] != "%HOME/.text-reminders;\"/di re ct or y\"" {t.Errorf("input mismatch. Expected: " + "%HOME/.text-reminders;\"/di re ct or y\"" + ". Got: " + res.inputs[1])}
+
+		default:
+			t.Errorf("Broke out of switch case")
+		}
 	}
 }
 
@@ -71,14 +85,45 @@ func writeReminderFile() (*os.File, error) {
 	file, err := os.Create("reminder.trem.test")
 	if err != nil {return nil, err}
 
-	// write file contents
+	file.Write([]byte(
+		"type=reminder\n" +
+		"date=monday\n" +
+		"reminder=Make sure to check your email\n",
+	))
 
 	_, err = file.Seek(0, 0)
-	if err != nil {return nil, err}
+	if err != nil {
+		file.Close()
+		return nil, err
+	}
 	return file, nil
 }
 func Test__ReadReminderFile(t *testing.T) {
 	file, remErr := writeReminderFile()
 	if remErr != nil {t.Errorf("%v", remErr)}
 	defer file.Close()
+
+	var funcmap map[string]func([]string) ReadFileResult = make(map[string]func([]string) ReadFileResult) // fill out later
+	results, readErr := ReadFile(file, funcmap)
+	if readErr != nil {t.Errorf("%v", readErr)}
+	if len(results) != 3 {t.Errorf("Result mismatch. Expected 3 results, got: %v", len(results))}
+
+	for idx, res := range results {
+		switch idx {
+		case 0:
+			if res.inputs[0] != "" {t.Errorf("keyword mismatch. Expected: type. Got: " + res.inputs[0])}
+			if res.inputs[1] != "" {t.Errorf("input mismatch. Expected: reminder. Got: " + res.inputs[1])}
+
+		case 1:
+			if res.inputs[0] != "" {t.Errorf("keyword mismatch. Expected: date. Got: " + res.inputs[0])}
+			if res.inputs[1] != "" {t.Errorf("input mismatch. Expected: monday. Got: " + res.inputs[1])}
+
+		case 2:
+			if res.inputs[0] != "" {t.Errorf("keyword mismatch. Expected: reminder. Got: " + res.inputs[0])}
+			if res.inputs[1] != "" {t.Errorf("input mismatch. Expected: Make sure to check your email. Got: " + res.inputs[1])}
+
+		default:
+			t.Errorf("")
+		}
+	}
 }
