2a3fdf1c01d2c026844e4598a454628b4f5999a5
Connor Walstrom <walst110@umn.edu>
2026-07-08 18:34:12 -0500

    Move some cli code out of trem main app

--- cmd/trem/main.go
+++ cmd/trem/main.go
@@ -2,12 +2,9 @@ package main
 
 import (
 	"github.com/realconebob/trem/internal/cli"
-	"github.com/realconebob/trem/internal/reminders"
 
-	"errors"
 	"fmt"
 	"os"
-	"time"
 )
 
 const DEFAULT_REMINDER_FILE string = "./REMINDERFILE.test"
@@ -22,18 +19,12 @@ func main() {
 
 	var err error
 	switch res.Command {
-	case cli.TC_ADD:
-		err = AddReminder(DEFAULT_REMINDER_FILE, res.Arguments)
-	case cli.TC_EDIT:
-		err = EditReminder(DEFAULT_REMINDER_FILE, res.Arguments)
-	case cli.TC_LIST:
-		err = ListReminders(DEFAULT_REMINDER_FILE, res.Arguments)
-	case cli.TC_REMOVE:
-		err = RemoveReminder(DEFAULT_REMINDER_FILE, res.Arguments)
-	case cli.TC_DAEMON:
-		err = CommandDaemon(DEFAULT_DAEMON_FILE, res.Arguments)
-	default:
-		err = res.Err
+	case cli.TC_ADD:	err = cli.AddReminder(DEFAULT_REMINDER_FILE, res.Arguments)
+	case cli.TC_EDIT:	err = cli.EditReminder(DEFAULT_REMINDER_FILE, res.Arguments)
+	case cli.TC_LIST:	err = cli.ListReminders(DEFAULT_REMINDER_FILE, res.Arguments)
+	case cli.TC_REMOVE:	err = cli.RemoveReminder(DEFAULT_REMINDER_FILE, res.Arguments)
+	case cli.TC_DAEMON:	err = cli.CommandDaemon(DEFAULT_DAEMON_FILE, res.Arguments)
+	default:			err = res.Err
 	}
 
 	if err != nil {
@@ -42,101 +33,4 @@ func main() {
 	}
 
 	os.Exit(0)
-}
-
-func AddReminder(file string, args []string) error {
-	currentReminders, err := reminders.GetFromGobFile(file)
-	if err != nil && !errors.Is(err, os.ErrNotExist) {
-		return err
-	}
-
-	layout := args[0]
-	switch layout {
-	case "Layout":
-		layout = time.Layout
-	case "ANSIC":
-		layout = time.ANSIC
-	case "UnixDate":
-		layout = time.UnixDate
-	case "RubyDate":
-		layout = time.RubyDate
-	case "RFC822":
-		layout = time.RFC822
-	case "RFC822Z":
-		layout = time.RFC822Z
-	case "RFC850":
-		layout = time.RFC850
-	case "RFC1123":
-		layout = time.RFC1123
-	case "RFC1123Z":
-		layout = time.RFC1123Z
-	case "RFC3339":
-		layout = time.RFC3339
-	case "RFC3339Nano":
-		layout = time.RFC3339Nano
-	case "Kitchen":
-		layout = time.Kitchen
-
-	case "Stamp":
-		layout = time.Stamp
-	case "StampMilli":
-		layout = time.StampMilli
-	case "StampMicro":
-		layout = time.StampMicro
-	case "StampNano":
-		layout = time.StampNano
-	case "DateTime":
-		layout = time.DateTime
-	case "DateOnly":
-		layout = time.DateOnly
-	case "TimeOnly":
-		layout = time.TimeOnly
-	} // Pulled straight from https://pkg.go.dev/time#Layout
-
-	// TODO: Make this more robust
-	datevalue := args[1]
-	message := args[2]
-	newReminder, err := reminders.CreateEntryByDate(layout, datevalue, message)
-	if err != nil {
-		return err
-	}
-
-	currentReminders = append(currentReminders, newReminder)
-	return reminders.SerializeToGobFile(currentReminders, file)
-}
-func EditReminder(file string, args []string) error {
-	currentReminders, err := reminders.GetFromGobFile(file)
-	if err != nil {
-		return err
-	}
-	currentReminders = currentReminders // Shut go up for the time being
-	// TODO: Implement
-
-	return nil
-}
-func ListReminders(file string, args []string) error {
-	reminders, err := reminders.GetFromGobFile(file)
-	if err != nil {
-		return err
-	}
-
-	for _, reminder := range reminders {
-		fmt.Println(reminder)
-	}
-
-	return nil
-}
-func RemoveReminder(file string, args []string) error {
-	currentReminders, err := reminders.GetFromGobFile(file)
-	if err != nil {
-		return err
-	}
-	currentReminders = currentReminders // Shut go up for the time being
-	// TODO: Implement
-
-	return nil
-}
-
-func CommandDaemon(file string, args []string) error {
-	return errors.New("TODO: main::CommandDaemon is unimplemented")
 }
--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -4,7 +4,10 @@ package cli
 // TODO: Finish the daemon then revisit this mess
 
 import (
+	"github.com/realconebob/trem/internal/reminders"
+
 	"errors"
+	"time"
 	"fmt"
 	"os"
 )
@@ -33,16 +36,11 @@ type CLIRes struct {
 func CreateCLIResFromArgs(command string, args []string) CLIRes {
 	var res CLIRes = CLIRes{Arguments: args}
 	switch command {
-	case "add":
-		res.Command = TC_ADD
-	case "edit":
-		res.Command = TC_EDIT
-	case "list":
-		res.Command = TC_LIST
-	case "remove":
-		res.Command = TC_REMOVE
-	case "daemon":
-		res.Command = TC_DAEMON
+	case "add":		res.Command = TC_ADD
+	case "edit":	res.Command = TC_EDIT
+	case "list":	res.Command = TC_LIST
+	case "remove":	res.Command = TC_REMOVE
+	case "daemon":	res.Command = TC_DAEMON
 	default:
 		res.Err = errors.New("Unknown command \"" + command + "\"")
 		res.Command = TC_UNKNOWN
@@ -63,4 +61,82 @@ func ProcCLIArgs() CLIRes {
 	}
 	workingArgs := os.Args[1:]
 	return CreateCLIResFromArgs(workingArgs[0], workingArgs[1:])
+}
+
+func AddReminder(file string, args []string) error {
+	currentReminders, err := reminders.GetFromGobFile(file)
+	if err != nil && !errors.Is(err, os.ErrNotExist) {
+		return err
+	}
+
+	layout := args[0]
+	switch layout {
+	case "Layout":		layout = time.Layout
+	case "ANSIC":		layout = time.ANSIC
+	case "UnixDate":	layout = time.UnixDate
+	case "RubyDate":	layout = time.RubyDate
+	case "RFC822":		layout = time.RFC822
+	case "RFC822Z":		layout = time.RFC822Z
+	case "RFC850":		layout = time.RFC850
+	case "RFC1123":		layout = time.RFC1123
+	case "RFC1123Z":	layout = time.RFC1123Z
+	case "RFC3339":		layout = time.RFC3339
+	case "RFC3339Nano":	layout = time.RFC3339Nano
+	case "Kitchen":		layout = time.Kitchen
+
+	case "Stamp":		layout = time.Stamp
+	case "StampMilli":	layout = time.StampMilli
+	case "StampMicro":	layout = time.StampMicro
+	case "StampNano":	layout = time.StampNano
+	case "DateTime":	layout = time.DateTime
+	case "DateOnly":	layout = time.DateOnly
+	case "TimeOnly":	layout = time.TimeOnly
+	} // Pulled straight from https://pkg.go.dev/time#Layout
+
+	// TODO: Make this more robust
+	datevalue := args[1]
+	message := args[2]
+	newReminder, err := reminders.CreateEntryByDate(layout, datevalue, message)
+	if err != nil {
+		return err
+	}
+
+	currentReminders = append(currentReminders, newReminder)
+	return reminders.SerializeToGobFile(currentReminders, file)
+}
+func EditReminder(file string, args []string) error {
+	currentReminders, err := reminders.GetFromGobFile(file)
+	if err != nil {
+		return err
+	}
+	currentReminders = currentReminders // Shut go up for the time being
+	// TODO: Implement
+
+	return nil
+}
+func ListReminders(file string, args []string) error {
+	reminders, err := reminders.GetFromGobFile(file)
+	if err != nil {
+		return err
+	}
+
+	for _, reminder := range reminders {
+		fmt.Println(reminder)
+	}
+
+	return nil
+}
+func RemoveReminder(file string, args []string) error {
+	currentReminders, err := reminders.GetFromGobFile(file)
+	if err != nil {
+		return err
+	}
+	currentReminders = currentReminders // Shut go up for the time being
+	// TODO: Implement
+
+	return nil
+}
+
+func CommandDaemon(file string, args []string) error {
+	return errors.New("TODO: main::CommandDaemon is unimplemented")
 }
