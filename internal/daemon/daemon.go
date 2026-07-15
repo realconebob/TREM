a5063944245bbf013d98679064c5be6abe431da7
Connor Walstrom <walst110@umn.edu>
2026-07-15 15:06:52 -0500

    Start implementing some conmmand behavior

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -3,55 +3,94 @@ package daemon
 // daemon.go - Background service that interacts with trem. Reads the reminder file and executes a reminder when the time is reached
 
 import (
-	"github.com/realconebob/trem/internal/reminders"
-	"github.com/realconebob/trem/internal"
 	"errors"
+	"fmt"
 	"time"
+
+	"github.com/realconebob/trem/internal"
+	"github.com/realconebob/trem/internal/reminders"
 )
 
-type TremDaemon struct {
-	Reminders []reminders.Entry
-	Dispatched map[uint64]reminders.Entry
-	CurrentTime time.Time
-	CommandFile *misc.WatchedFile
-	ReminderFile string
+type CommandIdx uint8
+const (
+	UNSPEC CommandIdx = iota
+	RELOAD_REMINDERS
+	RELOAD_CONFIG
+	SAVE_REMINDERS
+	PAUSE
+	SHUTDOWN
+	UNKNOWN
+)
+type Command struct {
+	Behavior CommandIdx
+	Path string
 }
 
-func CreateDaemonFromBuffer(data []byte) (TremDaemon, error) {
+type Config struct {
+	ReminderFile string
+	ConfigFile string
+}
+
+// Note: This struct is specifically for dealing with daemon state. Things not related to state belong in the config struct
+type Daemon struct {
+	Settings Config
+	Reminders []reminders.Entry
+	Dispatched map[uint64]reminders.Entry
+	CommandFile *misc.WatchedFile
+	paused bool
+	shutdown bool
+}
+
+func CreateDaemonFromBuffer(data []byte) (Daemon, error) {
 	rems, err := reminders.GetFromGob(data)
 
-	return TremDaemon{
+	return Daemon{
 		Reminders: rems,
 		Dispatched: make(map[uint64]reminders.Entry, 0),
-		CurrentTime: time.Now(),
 		CommandFile: &misc.WatchedFile{},
-		ReminderFile: "",
 	}, err
 }
 
-func CreateDaemonFromFile(cfile, rfile string, poll time.Duration) (TremDaemon, error) {
+func CreateDaemonFromFile(cfile, rfile string, poll time.Duration) (Daemon, error) {
 	rems, err := reminders.GetFromGobFile(rfile)
-	if err != nil {return TremDaemon{}, err}
+	if err != nil {return Daemon{}, err}
 
 	watch, err := misc.GetFileWatch(cfile, poll)
-	if err != nil {return TremDaemon{}, err}
+	if err != nil {return Daemon{}, err}
 
-	return TremDaemon{
+	return Daemon{
+		Settings: Config{
+			ReminderFile: rfile,
+		},
 		Reminders: rems,
 		Dispatched: make(map[uint64]reminders.Entry, 0),
-		CurrentTime: time.Now(),
 		CommandFile: watch,
-		ReminderFile: rfile,
 	}, nil
 }
 
-func (d *TremDaemon) DispatchReminders() {
+func (d *Daemon) InterpretCommand(cmd Command) error {
+	if d == nil {return errors.New("d is nil")}
+	switch cmd.Behavior {
+	case RELOAD_REMINDERS: 	return d.ReloadReminders()
+	case RELOAD_CONFIG:		return d.ReloadConfig(cmd.Path)
+	case SAVE_REMINDERS:	return d.SaveReminders(cmd.Path)
+	case PAUSE:				return d.Pause()
+	case SHUTDOWN:			return d.Close()
+	case UNSPEC:			return errors.New("Unspecified command")
+	case UNKNOWN: fallthrough
+	default: return errors.New("Unknown command: " + fmt.Sprint(cmd.Behavior))
+	}
+}
+
+func (d *Daemon) DispatchReminders() {
 	if d == nil {return}
 	for _, reminder := range d.Reminders {
 		if _, ok := d.Dispatched[reminder.Identifier]; ok {continue} // Much better
 		d.Dispatched[reminder.Identifier] = reminder
 
 		go func(cr *reminders.Entry){
+			if d.paused {return} // Don't bother if the daemon is paused
+
 			time.Sleep(time.Until(cr.TriggerOn))
 			cr.Triggered = true
 			// TODO: Emit a notification or pop up an application or window with the reminder
@@ -59,34 +98,50 @@ func (d *TremDaemon) DispatchReminders() {
 	}
 }
 
-func (d *TremDaemon) UpdateReminders() error {
+func (d *Daemon) ReloadReminders() error {
 	if d == nil {return errors.New("d is nil")}
 	// Update daemon with ReminderEntry-s from file
-	newReminders, err := reminders.GetFromGobFile(d.ReminderFile)
+	newReminders, err := reminders.GetFromGobFile(d.Settings.ReminderFile)
 	if err != nil {return err}
 	d.Reminders = newReminders
 
 	return nil
 }
 
-func (d *TremDaemon) Close() error {
+func (d *Daemon) Close() error {
 	if d == nil {return errors.New("d is nil")}
+	if d.shutdown == true {return errors.New("d has already been shutdown/closed")}
+
+	d.shutdown = true
 	d.CommandFile.Close()
-	return reminders.SerializeToGobFile(d.Reminders, d.ReminderFile)
+	return reminders.SerializeToGobFile(d.Reminders, d.Settings.ReminderFile)
 }
 
-func (d *TremDaemon) UpdateCommands() error {
+func (d *Daemon) UpdateCommands() error {
 	if d == nil {return errors.New("d is nil")}
-	return errors.New("<TremDaemon::UpdateCommands> Error: Not Implemented")
+	return errors.New("<Daemon::UpdateCommands> Error: Not Implemented")
 	// TODO: Implement
 }
-func (d *TremDaemon) RunCommands() error {
+func (d *Daemon) RunCommands() error {
 	if d == nil {return errors.New("d is nil")}
-	return errors.New("<TremDaemon::RunCommands> Error: Not Implemented")
+	return errors.New("<Daemon::RunCommands> Error: Not Implemented")
 	// TODO: Implement
 }
 
-func (d *TremDaemon) Run() {
+func (d *Daemon) ReloadConfig(path string) error {
+	// TODO:
+	panic("Unimplemented")
+}
+func (d *Daemon) SaveReminders(path string) error {
+	if path == "" {path = d.Settings.ReminderFile}
+	return reminders.SerializeToGobFile(d.Reminders, path)
+}
+func (d *Daemon) Pause() error {
+	d.paused = !d.paused
+	return nil
+}
+
+func (d *Daemon) Run() {
 	if d == nil {panic("d is nil")}
 	// The daemon should:
 		// *Get* populated with the current reminder file's reminders (This is not done by the daemon itself)
@@ -106,6 +161,7 @@ func (d *TremDaemon) Run() {
 	}()
 
 	for {
+		if d.shutdown {break}
 		<- fileUpdate
 		// TODO: Add error checking for these
 		d.UpdateCommands()
